7a7cbc18f3921b16361f01b3fc46ed067d768701
zrxing <zrxing@uchicago.edu>
2015-04-01 10:50:33 -0500

    nothing really

--- qsub_run_dsc.sh
+++ qsub_run_dsc.sh
@@ -5,7 +5,7 @@ if [ ! -d "${LOG_DIR}" ]; then
     mkdir ${LOG_DIR}
 fi
 
-PROCESS_NAME=`echo "dsc_smash"`
+PROCESS_NAME=`echo "run_dsc_smash"`
 echo "/data/tools/R-3.1.1/bin/Rscript run_dsc.R" | \
 qsub -l h_vmem=${MEM} -v PATH -cwd -N ${PROCESS_NAME} \
         -o ${LOG_DIR}${PROCESS_NAME}"_log.out" -e ${LOG_DIR}${PROCESS_NAME}"_log.err"
