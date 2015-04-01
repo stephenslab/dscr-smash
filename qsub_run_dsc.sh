53699ae522afcdf57fae487112dd627d1b3290f6
zrxing <zrxing@uchicago.edu>
2015-04-01 10:45:00 -0500

    updated qsub file

--- qsub_run_dsc.sh
+++ qsub_run_dsc.sh
@@ -1,5 +1,12 @@
 MEM=10g
 
+LOG_DIR="$HOME/dscr-smash/logs/"
+if [ ! -d "${LOG_DIR}" ]; then
+    mkdir ${LOG_DIR}
+fi
+
 PROCESS_NAME=`echo "dsc_smash"`
 echo "/data/tools/R-3.1.1/bin/Rscript run_dsc.R" | \
 qsub -l h_vmem=${MEM} -v PATH -cwd -N ${PROCESS_NAME} \
+        -o ${LOG_DIR}${PROCESS_NAME}"_log.out" -e ${LOG_DIR}${PROCESS_NAME}"_log.err"
+
