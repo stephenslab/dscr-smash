ef12fb53d4dac0e9c4be8294be93c073132324f0
Zhengrong Xing <zrxing@spudhead.localdomain>
2015-04-22 16:50:54 -0500

    modified ebayesthresh.wrapper

--- methods/ebayesthresh.wrapper.R
+++ methods/ebayesthresh.wrapper.R
@@ -6,25 +6,20 @@ library(wavethresh)
 
 #runs wavelet denoising using the Bayesian shrinkage/thresholding procedure EbayesThresh, assuming constant variance
 #inputs:
-#input: a list containing x: the data, sig.true the tr:ue sigma values, and sig.est: the estimated sigma values
+#input: a list containing x: the data, sig.true: the true sigma values, and sig.est: the estimated sigma values
 #args: a list containing family and filter.number, which determine the wavelet basis used
 #
-#returns the estimated (posterior median) mean function
+#returns the estimated (posterior mean) mean function
 ebayesthresh.wrapper = function(input,args){
   if(is.null(args$filter.number))
-    args$filter.number=10
+    args$filter.number=8
   if(is.null(args$family))
     args$family="DaubLeAsymm"
-  if(is.null(args$min.level))
-    args$min.level=3
 
   n=length(input$x)
   J=log2(n)
   x.w <- wd(input$x, args$filter.number, args$family, type = "station")
-  for(j in args$min.level:(J-1)){
-    x.pm = ebayesthresh(accessD(x.w,j),sdev=input$sig.est)
-    x.w = putD(x.w,j,x.pm)
-  }
+  x.w <- ebayesthresh.wavelet(x.w, vscale = noise.level, prior = "laplace", a = NA, threshrule = "mean")
   mu.est=AvBasis(convert(x.w))
   return(mu.est)
 }
