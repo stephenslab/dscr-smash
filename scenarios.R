ce1be38b9cdf2c23b4501e34c337c2b24e1d10a1
zrxing <zrxing@uchicago.edu>
2015-05-27 17:10:20 -0500

    first attempt at cleaning up code

--- scenarios.R
+++ scenarios.R
@@ -2,16 +2,17 @@ sourceDir("datamakers")
 
 library(AlgDesign)
 
-meanfn=c("spikes","bumps","blocks","angles","doppler","blip","corner")
+meanfn=c(spikes.fn,bumps.fn,bocks.fn,angles.fn,doppler.fn,blip.fn,cor.fn)
 meanfn.short=c("sp","bump","blk","ang","dop","blip","cor")
 rsnr=sqrt(c(1,3))
-varfn=1:5
+varfn=c(cons.fn,texp.fn,doppler.fn,bumps.fn,cblocks.fn)
+varfn.short=c("cons","texp","dop","bump","cblk")
 
 design=gen.factorial(c(length(varfn),length(rsnr),length(meanfn)),center=FALSE)
 
 for(i in 1:dim(design)[1]){
-  scenario.name=paste(meanfn.short[design[i,3]],rsnr[design[i,2]]^2,paste0("v",varfn[design[i,1]]),sep=".")
-  scenario.args=list(n=1024,meanfn=meanfn[design[i,3]],varfn=varfn[design[i,1]],rsnr=rsnr[design[i,2]])
+  scenario.name=paste(meanfn.short[design[i,3]],rsnr[design[i,2]]^2,varfn.short[design[i,1]],sep=".")
+  scenario.args=list(n=1024,rsnr=rsnr[design[i,2]],meanfn=meanfn[design[i,3]],varfn=varfn[design[i,1]])
   addScenario(dsc_smash,name=scenario.name,fn=datamaker,args=scenario.args,seed=1:100)
 }
 
